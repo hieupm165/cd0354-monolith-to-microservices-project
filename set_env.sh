a01822a6c13114eaa68061bc4ac100cad9c4ab57
hieu <hieupmhe130231@fpt.edu.vn>
2024-12-03 03:45:10 +0700

    commit

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=admin123
-export POSTGRES_HOST=postgres.czjmxzo9v7v3.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=host
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::project3-580625141506-dev
+export AWS_BUCKET=s3 bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
